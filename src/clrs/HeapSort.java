f255a4764fb2d70e5fdd1419104f6299f117c16f
sunzhengjie <200765821@qq.com>
2016-06-11 17:37:13 +0800

    heap_sort

--- src/clrs/HeapSort.java
+++ src/clrs/HeapSort.java
@@ -1,7 +1,52 @@
 package clrs;
 
+import org.junit.Test;
+
 /**
  * Created by szj on 2016/6/11.
  */
 public class HeapSort {
+
+    int[] origin = {9, 0, 8, 100, 6, 5, 4, 11, 2, 1};
+
+    @Test
+    public void buildMaxHeap() {
+        for (int i = (origin.length - 1) / 2; i >= 0; i--) {
+            maxHeapify(i);
+        }
+        //only print
+        for (int x : origin) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public void maxHeapify(int index) {
+        int leftSonIndex = left(index);
+        int rightSonIndex = right(index);
+        int largestIndex = -1;
+        //index not out of heap size
+        if (leftSonIndex < origin.length
+                && origin[index] < origin[leftSonIndex]) {
+            largestIndex = leftSonIndex;
+        }
+        if (rightSonIndex < origin.length
+                && origin[index] < origin[rightSonIndex]) {
+            largestIndex = rightSonIndex;
+        }
+        if(largestIndex == -1){
+            return;
+        }
+        int temp = origin[index];
+        origin[index] = origin[largestIndex];
+        origin[largestIndex] = temp;
+        maxHeapify(largestIndex);
+    }
+
+    public int left(int index) {
+        return index * 2;
+    }
+
+    public int right(int index) {
+        return index * 2 + 1;
+    }
 }
