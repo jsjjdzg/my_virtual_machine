18ec4bd1a75147b3b9bf091ce91816c59456390d
szj <200765821@qq.com>
2016-06-13 16:03:28 +0800

    radixSort
    remenber full in c[]  must from the end to the ahead

--- src/clrs/RadixSort.java
+++ src/clrs/RadixSort.java
@@ -6,27 +6,21 @@ import org.junit.Test;
  * Created by szj on 2016/6/13.
  */
 public class RadixSort {
-    int[] origin = {101, 201, 423, 453, 123, 356, 765, 56, 788};
-    int d = 3;
-    private int[] b = new int[11]; // output
-    private int[] c = new int[1000]; // count how many numbers smaller than it
-
+    private int[] origin = {201, 113, 423, 461, 123, 356, 165, 56, 781};
+    private int[] c = new int[9]; // count how many numbers smaller than it
     @Test
     public void main() {
         radixSort();
     }
 
     public void radixSort() {
-        countSortOne(origin);
-        countSortTen(origin);
-        countSortHundred(origin);
-        for(int i = 0; i < origin.length; i++){
-
-            System.out.print(origin[i] + " ");
+        for(int x : countSortHundred(countSortTen(countSortOne(origin)))){
+            System.out.print(x + " ");
         }
     }
 
-    public void countSortOne(int[] sort) {
+    public int[] countSortOne(int[] sort) {
+        int[] b = new int[9]; // output
         // how many
         for (int i = 0; i < sort.length; i++) {
             c[sort[i] % 10] += 1;
@@ -35,37 +29,44 @@ public class RadixSort {
         for (int i = 1; i < c.length; i++) {
             c[i] = c[i] + c[i - 1];
         }
-        for (int i = 0; i < sort.length; i++) {
-            b[c[sort[i] % 10]] = sort[i];
-            c[sort[i]] -= 1;
+        for(int i = sort.length - 1; i > -1; i--){
+            b[c[sort[i] % 10] - 1] = sort[i];
+            c[sort[i] % 10] -= 1;
         }
+        return b;
     }
-    public void countSortTen(int[] sort){
-        // how many
+    public int[] countSortTen(int[] sort){
+        int[] b = new int[9]; // output
+        c = new int[9];
+        // count decade
         for(int i = 0; i < sort.length; i++){
-            c[sort[i] % 100 - sort[i] % 10] += 1;
+            c[(sort[i]%100)/10] += 1;
         }
         // how many low or equal
         for(int i = 1; i < c.length; i++){
             c[i] = c[i] + c[i-1];
         }
-        for(int i = 0; i < sort.length; i++){
-            b[c[sort[i] % 100 - sort[i] % 10]] = sort[i];
-            c[sort[i] % 100 - sort[i] % 10] -= 1;
+        for(int i = sort.length - 1; i > -1; i--){
+            b[c[(sort[i]%100)/10] - 1] = sort[i];
+            c[(sort[i]%100)/10] -= 1;
         }
+        return b;
     }
-    public void countSortHundred(int[] sort){
+    public int[] countSortHundred(int[] sort){
+        int[] b = new int[9]; // output
+        c = new int[9];
         // how many
         for(int i = 0; i < sort.length; i++){
-            c[sort[i] - sort[i] % 100] += 1;
+            c[sort[i]/100] += 1;
         }
         // how many low or equal
         for(int i = 1; i < c.length; i++){
             c[i] = c[i] + c[i-1];
         }
-        for(int i = 0; i < sort.length; i++){
-            b[c[sort[i] - sort[i] % 100]] = sort[i];
-            c[sort[i] - sort[i] % 100] -= 1;
+        for(int i = sort.length - 1; i > -1; i--){
+            b[c[sort[i]/100] - 1] = sort[i];
+            c[sort[i]/100] -= 1;
         }
+        return b;
     }
 }
